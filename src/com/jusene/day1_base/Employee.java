37925185d774ea0daf127783a088b640056ceb08
zhangguoxing <zhangguoxing@youzan.com>
2021-07-12 23:22:29 +0800

    day1

--- src/com/jusene/day1_base/Employee.java
+++ src/com/jusene/day1_base/Employee.java
@@ -1,7 +1,15 @@
 package com.jusene.day1_base;
 
-import java.io.*;
-
 public class Employee {
+    static int a = 1;
+    static int b = 3;
 
+    public int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+         int sum = new Employee().Sum(a, b);
+         System.out.println(sum);
+    }
 }
--- src/com/jusene/day1_base/Puppy.java
+++ src/com/jusene/day1_base/Puppy.java
@@ -5,6 +5,11 @@ public class Puppy{
         //这个构造器仅有一个参数：name
         System.out.println("小狗的名字是 : " + name );
     }
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args){
         // 下面的语句将创建一个Puppy对象
         Puppy myPuppy = new Puppy( "tommy" );
